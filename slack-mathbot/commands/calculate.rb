4fcbd3910a5820f2ccce6396ef51b25c1c1c04eb
sjvargas <sjvargas@uc.cl>
2016-06-21 00:44:48 -0400

    vfdvasasaasdasdasdasdas

--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -30,8 +30,8 @@ module SlackMathbot
   module Commands
     class Calculate < SlackRubyBot::Commands::Base
       command 'calculate' do |client, data, _match|
-        origen1 = data.text
-        destino1 = data.text
+        #origen1 = data.text
+        #destino1 = data.text
        # busqueda = RestClient.get 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + origen1.split(­'calculate')[1].sp­lit('-')[0] + '&destinations=' + destino1.split(­'calculate')[1].sp­lit('-')[1] + '&key= AIzaSyBSIU35JErNsPB9fClZmbhah7xowIwVfno '
        # busquedaJson = JSON.parse(busqueda)
        # origen = busquedaJson["origin_addresses"][0].to_s
@@ -40,7 +40,7 @@ module SlackMathbot
        # tiempo = busquedaJson['rows'][0]['elements'][0]['duration']['text'].to_s
 
 #        client.say(channel: data.channel, text: "Calculando distancia y tiempo")
-      client.say(channel: data.channel, text: 'meee')
+      client.say(channel: data.channel, text: data.text)
        #client.say(channel: data.channel, text: origen)
        # client.say(channel: data.channel, text: destino)
        # client.say(channel: data.channel, text: distancia)
