b0d1b44c95a468521e468e2d535e5726f858690b
sjvargas <sjvargas@uc.cl>
2016-06-21 12:15:05 -0400

    prueba3

--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -32,22 +32,23 @@ module SlackMathbot
       command 'calculate' do |client, data, _match|
        # origen1 = data.text
         destino1 = data.text
-        #s = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + origen1.split(­'calculate')[1].sp­lit('-')[0] + '&destinations=' + destino1.split(­'calculate')[1].sp­lit('-')[1] + '&key= AIzaSyBSIU35JErNsPB9fClZmbhah7xowIwVfno '
+        s = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=Santiago' + '&destinations=' + destino1[9..-1]+ '&key= AIzaSyBSIU35JErNsPB9fClZmbhah7xowIwVfno '
         client.say(channel: data.channel, text: destino1[9..-1])
 
-        #busqueda = RestClient.get s
-        #busquedaJson = JSON.parse(busqueda)
-        #origen = busquedaJson["origin_addresses"][0].to_s
-        #destino = busquedaJson["destination_addresses"][0].to_s
-        #distancia = busquedaJson['rows'][0]['elements'][0]['distance']['text'].to_s
-        #tiempo = busquedaJson['rows'][0]['elements'][0]['duration']['text'].to_s
+        busqueda = RestClient.get s
+        busquedaJson = JSON.parse(busqueda)
+        
+        origen = busquedaJson["origin_addresses"][0].to_s
+        destino = busquedaJson["destination_addresses"][0].to_s
+        distancia = busquedaJson['rows'][0]['elements'][0]['distance']['text'].to_s
+        tiempo = busquedaJson['rows'][0]['elements'][0]['duration']['text'].to_s
 
 #        client.say(channel: data.channel, text: "Calculando distancia y tiempo")
        client.say(channel: data.channel, text: data.text)
-       #client.say(channel: data.channel, text: origen)
-       # client.say(channel: data.channel, text: destino)
-       # client.say(channel: data.channel, text: distancia)
-       # client.say(channel: data.channel, text: tiempo)
+       client.say(channel: data.channel, text: origen)
+       client.say(channel: data.channel, text: destino)
+       client.say(channel: data.channel, text: distancia)
+       client.say(channel: data.channel, text: tiempo)
       end        
     end
   end
