1ac25ed48a82a34ca5cf8136fa4ff7b8ae816f72
sjvargas <sjvargas@uc.cl>
2016-06-20 23:09:32 -0400

    nuevo avance7

--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -3,7 +3,7 @@ require 'rest-client'
 module SlackMathbot
   module Commands
     class Distancia < SlackRubyBot::Commands::Base
-      command 'Distancia entre' do |client, data, _match|
+      command 'Distancia entre:' do |client, data, _match|
         client.say(channel: data.channel, text: "Calculando distancia y tiempo")
         origen1 = data.text.split(­':')[1].sp­lit('-')
         destino1 = data.text.split(­':')[1].sp­lit('-')
