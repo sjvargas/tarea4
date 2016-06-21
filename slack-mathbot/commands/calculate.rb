0a3855342e124c61524896e059e217bcf917d4c7
sjvargas <sjvargas@uc.cl>
2016-06-20 23:15:36 -0400

    gran avance

--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -2,15 +2,25 @@ require 'rest-client'
 
 module SlackMathbot
   module Commands
-    class Distancia < SlackRubyBot::Commands::Base
+    class Tweets < SlackRubyBot::Commands::Base
       command 'Distancia entre:' do |client, data, _match|
-        client.say(channel: data.channel, text: "Calculando distancia y tiempo")
         origen1 = data.text.split(­':')[1].sp­lit('-')
-        destino1 = data.text.split(­':')[1].sp­lit('-')
-        client.say(channel: data.channel, text: origen1[0])
-        client.say(channel: data.channel, text: destino1[1])
+        destino1 = data.text.split(­':')[1].sp­lit('-') 
 
-			end        
+        busqueda = RestClient.get  'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + origen1[0] + '&destinations=' + destino1[1] + '&key= AIzaSyBSIU35JErNsPB9fClZmbhah7xowIwVfno'
+        busquedaJson = JSON.parse(busqueda)
+        origen = busquedaJson["origin_addresses"][0].to_s
+        destino = busquedaJson["destination_addresses"][0].to_s
+        distancia = busquedaJson['rows'][0]['elements'][0]['distance']['text'].to_s
+        tiempo = busquedaJson['rows'][0]['elements'][0]['duration']['text'].to_s
+
+        client.say(channel: data.channel, text: "Calculando distancia y tiempo")
+
+        client.say(channel: data.channel, text: origen)
+        client.say(channel: data.channel, text: destino)
+        client.say(channel: data.channel, text: distancia)
+        client.say(channel: data.channel, text: tiempo)
+      end        
     end
   end
 end
