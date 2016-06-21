973a85e3ae92489a7fe978afcdbb1ad9f9baa54c
sjvargas <sjvargas@uc.cl>
2016-06-20 23:40:33 -0400

    cambio dist4

--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -2,12 +2,11 @@ require 'rest-client'
 
 module SlackMathbot
   module Commands
-    class Distancia< SlackRubyBot::Commands::Base
-      command 'Distancia entre' do |client, data, _match|
-        origen1 = data.text.split(­':')[1].sp­lit('-')
-        destino1 = data.text.split(­':')[1].sp­lit('-') 
-
-        busqueda = RestClient.get  'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + origen1[0] + '&destinations=' + destino1[1] + '&key=AIzaSyBSIU35JErNsPB9fClZmbhah7xowIwVfno'
+    class Distancia < SlackRubyBot::Commands::Base
+      command 'Distancia entre:' do |client, data, _match|
+        origen1 = data.text
+        destino1 = data.text
+        busqueda = RestClient.get 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + origen1.split(­':')[1].sp­lit('-')[0] + '&destinations=' + destino1.split(­':')[1].sp­lit('-')[1] + '&key= AIzaSyBSIU35JErNsPB9fClZmbhah7xowIwVfno '
         busquedaJson = JSON.parse(busqueda)
         origen = busquedaJson["origin_addresses"][0].to_s
         destino = busquedaJson["destination_addresses"][0].to_s
