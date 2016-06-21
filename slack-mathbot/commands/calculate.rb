789719722f87d8edae828fe09b0223978548cab7
sjvargas <sjvargas@uc.cl>
2016-06-21 00:47:15 -0400

    vfdvasasaasdasdasdasdassdasdaasdasd

--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -32,12 +32,12 @@ module SlackMathbot
       command 'calculate' do |client, data, _match|
         origen1 = data.text
         destino1 = data.text
-       # busqueda = RestClient.get 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + origen1.split(­'calculate')[1].sp­lit('-')[0] + '&destinations=' + destino1.split(­'calculate')[1].sp­lit('-')[1] + '&key= AIzaSyBSIU35JErNsPB9fClZmbhah7xowIwVfno '
-       # busquedaJson = JSON.parse(busqueda)
-       # origen = busquedaJson["origin_addresses"][0].to_s
-       # destino = busquedaJson["destination_addresses"][0].to_s
-       # distancia = busquedaJson['rows'][0]['elements'][0]['distance']['text'].to_s
-       # tiempo = busquedaJson['rows'][0]['elements'][0]['duration']['text'].to_s
+        busqueda = RestClient.get 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + origen1.split(­'calculate')[1].sp­lit('-')[0] + '&destinations=' + destino1.split(­'calculate')[1].sp­lit('-')[1] + '&key= AIzaSyBSIU35JErNsPB9fClZmbhah7xowIwVfno '
+        busquedaJson = JSON.parse(busqueda)
+        origen = busquedaJson["origin_addresses"][0].to_s
+        destino = busquedaJson["destination_addresses"][0].to_s
+        distancia = busquedaJson['rows'][0]['elements'][0]['distance']['text'].to_s
+        tiempo = busquedaJson['rows'][0]['elements'][0]['duration']['text'].to_s
 
 #        client.say(channel: data.channel, text: "Calculando distancia y tiempo")
       client.say(channel: data.channel, text: data.text)
