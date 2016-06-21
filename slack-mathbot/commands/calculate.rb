9a19f3785cc5492d9c40f0563a58abb1458ed6cc
sjvargas <sjvargas@uc.cl>
2016-06-21 00:10:29 -0400

    cambio dist7

--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -1,24 +1,50 @@
+#require 'rest-client'
+
+#module SlackMathbot
+#  module Commands
+#    class Distancia < SlackRubyBot::Commands::Base
+#      command 'Distancia entre:' do |client, data, _match|
+#        origen1 = data.text
+#        destino1 = data.text
+#        busqueda = RestClient.get 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + origen1.split(­':')[1].sp­lit('-')[0] + '&destinations=' + destino1.split(­':')[1].sp­lit('-')[1] + '&key= AIzaSyBSIU35JErNsPB9fClZmbhah7xowIwVfno '
+#        busquedaJson = JSON.parse(busqueda)
+#        origen = busquedaJson["origin_addresses"][0].to_s
+#        destino = busquedaJson["destination_addresses"][0].to_s
+#        distancia = busquedaJson['rows'][0]['elements'][0]['distance']['text'].to_s
+#        tiempo = busquedaJson['rows'][0]['elements'][0]['duration']['text'].to_s
+#
+#        client.say(channel: data.channel, text: "Calculando distancia y tiempo")
+#
+#        client.say(channel: data.channel, text: origen)
+#        client.say(channel: data.channel, text: destino)
+#        client.say(channel: data.channel, text: distancia)
+#        client.say(channel: data.channel, text: tiempo)
+#      end        
+#    end
+#  end
+#end
+
 require 'rest-client'
 
 module SlackMathbot
   module Commands
     class Calculate < SlackRubyBot::Commands::Base
-      command 'Distancia entre:' do |client, data, _match|
+      command 'calculate' do |client, data, _match|
         origen1 = data.text
         destino1 = data.text
-        busqueda = RestClient.get 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + origen1.split(­':')[1].sp­lit('-')[0] + '&destinations=' + destino1.split(­':')[1].sp­lit('-')[1] + '&key= AIzaSyBSIU35JErNsPB9fClZmbhah7xowIwVfno '
+        busqueda = RestClient.get 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + origen1.split(­'calculate')[1].sp­lit('-')[0] + '&destinations=' + destino1.split(­'calculate')[1].sp­lit('-')[1] + '&key= AIzaSyBSIU35JErNsPB9fClZmbhah7xowIwVfno '
         busquedaJson = JSON.parse(busqueda)
         origen = busquedaJson["origin_addresses"][0].to_s
-        #destino = busquedaJson["destination_addresses"][0].to_s
-        #distancia = busquedaJson['rows'][0]['elements'][0]['distance']['text'].to_s
-        #tiempo = busquedaJson['rows'][0]['elements'][0]['duration']['text'].to_s
+        destino = busquedaJson["destination_addresses"][0].to_s
+        distancia = busquedaJson['rows'][0]['elements'][0]['distance']['text'].to_s
+        tiempo = busquedaJson['rows'][0]['elements'][0]['duration']['text'].to_s
 
-        #client.say(channel: data.channel, text: "Calculando distancia y tiempo")
+        client.say(channel: data.channel, text: "Calculando distancia y tiempo")
 
         client.say(channel: data.channel, text: origen)
-#        client.say(channel: data.channel, text: destino)
-#        client.say(channel: data.channel, text: distancia)
-#        client.say(channel: data.channel, text: tiempo)
+        client.say(channel: data.channel, text: destino)
+        client.say(channel: data.channel, text: distancia)
+        client.say(channel: data.channel, text: tiempo)
       end        
     end
   end
